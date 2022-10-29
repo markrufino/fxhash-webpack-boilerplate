53fb73de12ea837fbfe9d284d6494a27c649e9d5
Mark Rufino <mark.rufino.io@gmail.com>
2022-10-30 06:21:54 +0800

    wip

--- src/index.js
+++ src/index.js
@@ -20,10 +20,36 @@ console.log(fxrand()) // deterministic PRNG function, use it instead of Math.ran
 //   "Inverted": true
 // }
 
+window.$fxhashFeatures = {
+  "Background": "Black",
+  "Number of lines": 10,
+  "Inverted": true
+}
+
 // this code writes the values to the DOM as an example
+const words = "abracadabra abracadabra abracadabra abracadabra abracadabra d".split(" ")
 const container = document.createElement("div")
-container.innerText = `
-  random hash: ${fxhash}\n
-  some pseudo random values: [ ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()},... ]\n
+const x = fxrand()
+// container.innerText = `
+//   random hash: ${fxhash}\n
+//   some pseudo random values: [ ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()}, ${fxrand()},... ]\n
+// `
+content = ""
+
+words.forEach((word, i, arr) => {
+  const size = fxrand() * 7
+  content += `
+  <div>
+  ${word.split('').map((c) => { return `<font size='${fxrand() * 20}' color='${fxrand() * 0xFFFFFF << 0}'>${c}</font>` }).join('')}
+  </div>
+  `
+})
+
+
+container.innerHTML = `
+<div class = "content">
+${content}
+</div>
 `
+
 document.body.prepend(container)
